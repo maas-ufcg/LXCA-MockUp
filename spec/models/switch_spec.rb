ad2bda776203a3e94fa8160a466da3fa9765c2c9
Walter Alves <walter.arruda.alves@gmail.com>
2016-08-22 16:29:00 -0300

    Base test implementation.

--- spec/models/switch_spec.rb
+++ spec/models/switch_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Switch, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Model validations" do
+    before :each do
+      @swich = build :switch
+      @switch_hasnt_uuid = build :inv_switch_hasnt_uuid
+      @switch_different_id_and_uuid = build :inv_switch_different_id_and_uuid
+    end
+
+
+    it "is invalid if has no uuid in properties" do
+      expect(@switch_hasnt_uuid).to_not be_valid(Switch)
+    end
+
+    it "is invalid if _id is different from properties.uuid" do
+      expect(@switch_different_id_and_uuid).to_not be_valid(Switch)
+    end
+
+    it "a valid Switch can be saved" do
+      expect(@switch).to be_valid(Switch)
+    end
+
+  end
+
 end
