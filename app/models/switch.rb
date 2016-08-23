2ed1f78b33a93ddd0ebd55e7649320699c1c034a
Odravison <oamaraljr@gmail.com>
2016-08-23 15:34:58 -0300

    Setting model to run validators

--- app/models/switch.rb
+++ app/models/switch.rb
@@ -1,5 +1,9 @@
+require "../validators/switch_validators"
+
 class Switch
   include Mongoid::Document
   field :_id, type: String
   field :properties, type: Hash
+
+  validates_with SwitchValidators  
 end
