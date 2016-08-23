3b59fbf8fcf70e9eb4f07dc6f775c2b46945cf16
maysasouza <maysa.souza@ccc.ufcg.edu.br>
2016-08-23 16:02:17 -0300

    Modelo

--- spec/factories/power_supplies.rb
+++ spec/factories/power_supplies.rb
@@ -24,6 +24,8 @@ FactoryGirl.define do
 
 led_states = %w(Off On Blinking Unknown)
 
+power_states = %w(Off On ShuttingDown Standby Hibernate Unknown)
+
 factory:power_supply_properties: Hash do
   cmmDisplayName {Faker::Hipster.word}
   cmmHealthState{health_states.sample}
@@ -41,9 +43,40 @@ factory:power_supply_properties: Hash do
   fruSerialNumber ""
   hardwareRevision ""
   inputVoltageIsAC {[true,false].sample}
+  inputVoltageMax {Random.rand -1...2**64}
+  inputVoltageMin {Random.rand -1...2**64}
+  leds do
+       (0..5).map do |number|
+         {
+           "color" => led_colors.sample,
+           "location" => led_locations.sample,
+           "name" => Faker::Lorem.word,
+           "state" => led_states.sample
+         }
+       end
+    end
+    machineType { Random.rand 0...2**64 }
+    manufacturer ""
+    manufactureDate { "#{Random.rand 0...10000}" }
+    manufacturerID ""
+    model ""
+    name { Faker::Hipster.word }
 
-end
+    partNumber ""
+    posID "" # Special Attention
+    powerState { power_states.sample }
 
+    productID "" # Special Attention
+    productName { Faker::App.name }
+    serialNumber { "US#{Random.rand 1000...100000}S"}
+    slots 1 # Special attention
+    type "PowerSupply"
+    uri { Faker::Internet.url }
+    userDescription { Faker::Lorem.sentence }
+    uuid { SecureRandom.uuid }
+    vpdID "" # Special attention
 
+    initialize_with { attributes }
 
+    end
 end
