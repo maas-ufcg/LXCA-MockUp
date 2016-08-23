bca4545649c28630a8daae480a062fecf7af6696
Walter Alves <walter.arruda.alves@gmail.com>
2016-08-23 09:13:29 -0300

    Test case Switch missing some property.

--- spec/models/switch_spec.rb
+++ spec/models/switch_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe Switch, type: :model do
   describe "Model validations" do
     before :each do
       @switch = build :switch
-      @switch_hasnt_uuid = build :inv_switch_hasnt_uuid
+      @switch_missing_uuid = build :inv_switch_missing_uuid
       @switch_different_id_and_uuid = build :inv_switch_different_id_and_uuid
+      @switch_missing_property = build :inv_switch_missing_property
     end
 
 
@@ -19,9 +20,16 @@ RSpec.describe Switch, type: :model do
       expect(@switch_different_id_and_uuid).to_not be_valid(Switch)
     end
 
+    it "is invalid if some property is missing" do
+      expect(@switch_missing_property).to_not be_valid(Switch)
+    end
+
     it "a valid Switch can be saved" do
       expect(@switch).to be_valid(Switch)
     end
+
+
+
 
   end
 
