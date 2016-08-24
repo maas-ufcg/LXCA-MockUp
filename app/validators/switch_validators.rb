07eeb1bcf3c948b7c1a51a0bc831ea4a5c206dba
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2016-08-24 05:39:25 -0300

    Added tests in switch_validators.rb, including _id and uuid

--- app/validators/switch_validators.rb
+++ app/validators/switch_validators.rb
@@ -1,7 +1,20 @@
+require "../models/node"
+
 class SwitchValidators < ActiveModel::Validator
 	def validate(record)
+		validate_uuid_and_id(record)
 		validate_access_state(record)
 		validate_apply_pending(record)
+		validate_attached_nodes(record)
+		validate_display_name(record)
+		validate_health_state(record)
+		validate_contact(record)
+		validate_cpu_utilization(record)
+		validate_data_handle(record)
+		validate_description(record)
+		validate_dns_host_names(record)
+		validate_domain_name(record)
+		validate_entitle_serial_number(record_v)
 	end
 
 	private
@@ -32,6 +45,7 @@ class SwitchValidators < ActiveModel::Validator
 		elsif not record_v.properties[:attachedNodes].is_a? Array
 			record_v.errors[:base] << "AttachedNodes attribute must be an array of nodes values"
 		end
+		validates_associated :node
 	end
 
 	def validate_display_name(record_v)
@@ -55,4 +69,85 @@ class SwitchValidators < ActiveModel::Validator
 			record_v.errors[:base] << "CMMHealthState attribute is not valid"
 		end
 	end
+
+	def validate_uuid_and_id(record_v)
+		if record_v == nil
+			record_v.errors[:base] << "UUID attribute can't be nil"
+		elsif record_v.propeties[:uuid] == "" or record_v.propeties[:uuid] == nil
+			record_v.errors[:base] << "UUID attribute can't be nil or empty"
+		elsif record_v.propeties[:uuid] != record_v._id #Special Attention: Check access to the _id value in the switch model
+			record_v.errors[:base] << "UUID attribute must be equal to the id attribute in the switch"
+		end
+	end
+
+	def validate_contact(record_v)
+		if record_v == nil
+			record_v.errors[:base] << "Contact attribute can't be nil"
+		elsif not record_v.propeties[:contact].is_a? String
+			record_v.errors[:base] << "Contact attribute must be of the type: String"
+		elsif record_v.propeties[:contact] == ""
+			record_v.errors[:base] << "Contact attribute can't be empty"
+		end
+	end
+
+	def validate_cpu_utilization(record_v)
+		if record_v == nil
+			record_v.errors[:base] << "CPU Utilization attribute can't be nil"
+		elsif not record_v.properties[:cpuUtilization].is_a? Long
+			record_v.errors[:base] << "CPU Utilization attribute must be a Long numeric value"
+		elsif record_v.properties[:cpuUtilization] < 0
+			record_v.errors[:base] << "CPU Utilization attribute can't be negative"
+		end
+	end
+
+	def validate_data_handle(record_v)
+		if record_v == nil
+			record_v.errors[:base] << "Data Handle attribute can't be nil"
+		elsif not record_v.properties[:dataHandle].is_a? Long
+			record_v.errors[:base] << "Data Handle attribute must be a Long numeric value"
+		elsif record_v.properties[:dataHandle] < 0
+			record_v.errors[:base] << "Data Handle attribute can't be negative"
+		end
+	end
+
+	def validate_description(record_v)
+		if record_v == nil
+			record_v.errors[:base] << "Description attribute can't be nil"
+		elsif not record_v.properties[:description].is_a? String
+			record_v.errors[:base] << "Description attribute must be os the type: String"
+		elsif record_v.properties[:description] == ""
+			record_v.errors[:base] << "Description attribute can't be empty"
+		end
+	end
+
+	def validate_dns_host_names(record_v)
+		if record_v == nil
+			record_v.errors[:base] << "DNSHostnames attribute can't be nil"
+		elsif not record_v.properties[:dnsHostnames].is_a? Array
+			record_v.errors[:base] << "DNSHostnames attribute must be an Array of String values"
+		else
+			for dns in record_v.properties[:dnsHostnames]
+				if not dns.is_a? String
+					record_v.errors[:base] << "DNS Hostname attribute must contain only String values"
+				end
+			end
+		end
+	end
+
+	def validate_domain_name(record_v)
+		if record_v == nil
+			record_v.errors[:base] << "Domain Name attribute can't be nil"
+		elsif not record_v.properties[:domainName].is_a? String
+			record_v.errors[:base] << "Domain Name attribute must be a String value"
+		end
+	end
+
+	def validate_entitle_serial_number(record_v)
+		if  record_v == nil
+			record_v.errors[:base] << "Entitle Serial Number attribute can't be nil"
+		elsif not record_v.properties[:entitleSerialNumber].is_a? String
+			record_v.errors[:base] << "Entitle Serial Number attribute must be of the type: String"
+		end
+	end
+
 end
