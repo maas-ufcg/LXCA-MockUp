1ef801921863d6fe0dc00e01e7a1f96c88bbbda9
maysasouza <maysa.souza@ccc.ufcg.edu.br>
2016-08-30 17:48:51 -0300

    Adding controller's test for Nodes

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -4,67 +4,76 @@ class NodesController < ApplicationController
   # GET /nodes
   # GET /nodes.json
   def index
-    @nodes = Node.all
+
+    param = params[:status]
+    if param.nil?
+      @nodes = Node.all.to_a
+    else
+      @nodes = Node.all.to_a.select do |node|
+        node.properties.to_hash.deep_symbolize_keys[:status][:message] == param
+      end
+    end
+    render(json: @nodes.map do |node|
+      setup_node_properties node
+      node.properties
+    end)
   end
 
   # GET /nodes/1
   # GET /nodes/1.json
-  def show
-  end
+#   def show
+#     if @node.nil?
+#       head :not_found
+#     else
+#       render json: @node.properties
+#     end
+# end
 
-  # GET /nodes/new
-  def new
-    @node = Node.new
-  end
-
-  # GET /nodes/1/edit
-  def edit
-  end
-
-  # POST /nodes
-  # POST /nodes.json
-  def create
-    @node = Node.new(node_params)
-
-    respond_to do |format|
-      if @node.save
-        format.html { redirect_to @node, notice: 'Node was successfully created.' }
-        format.json { render :show, status: :created, location: @node }
-      else
-        format.html { render :new }
-        format.json { render json: @node.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PATCH/PUT /nodes/1
   # PATCH/PUT /nodes/1.json
-  def update
-    respond_to do |format|
-      if @node.update(node_params)
-        format.html { redirect_to @node, notice: 'Node was successfully updated.' }
-        format.json { render :show, status: :ok, location: @node }
-      else
-        format.html { render :edit }
-        format.json { render json: @node.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @node.update(node_params)
+  #       format.html { redirect_to @node, notice: 'Node was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @node }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @node.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /nodes/1
-  # DELETE /nodes/1.json
-  def destroy
-    @node.destroy
-    respond_to do |format|
-      format.html { redirect_to nodes_url, notice: 'Node was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_node
-      @node = Node.find(params[:id])
+      @node = begin
+          Node.find(params[:id])
+        rescue Mongoid::Errors::DocumentNotFound => ex
+        end
+        setup_node_properties @node
+    end
+
+    def setup_node_properties(node)
+      return if node.nil?
+      excludeAttributes = split_to_sym params[:excludeAttributes]
+      includeAttributes = split_to_sym params[:includeAttributes]
+
+
+
+      unless includeAttributes.nil?
+        excludeAttributes = NodesHelper::required_fields-includeAttributes
+      end
+      unless excludeAttributes.nil?
+        excludeAttributes.each do |attribute|
+          node.properties.delete(attribute)
+        end
+      end
+    end
+
+    def split_to_sym(string)
+      string.split(",").map(&:to_sym) if string.is_a?(String)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
