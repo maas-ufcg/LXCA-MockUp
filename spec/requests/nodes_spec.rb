4fd2566ba5de95b142ab55587671f139d2f830b5
gabrielvinha <gabriel.vinha@ccc.ufcg.edu.br>
2016-09-01 12:28:50 -0300

    Check tests of type return. All other request tests implemented :)

--- spec/requests/nodes_spec.rb
+++ spec/requests/nodes_spec.rb
@@ -88,6 +88,7 @@ RSpec.describe "Nodes", type: :request do
 
         )
         get nodes_path, {excludeAttributes: @excludeAttributes.join(",")}
+        @fetched_node = JSON.parse(response.body)
       end
 
       it "HTTP response code is 200 (OK)" do
@@ -109,6 +110,117 @@ RSpec.describe "Nodes", type: :request do
       it "All items in the array should be a hash object" do
         JSON.parse(response.body).each do |object|
           expect(object).to be_a(Hash)
+        end
+      end
+
+      it "All items in response shouldn't have any of the specified attributes" do
+        @excludeAttributes.each do |attribute|
+          expect(@fetched_node.has_key? attribute.to_s).to eq(false)
+        end
+      end
+    end
+  end
+
+
+  describe "GET /nodes/:uuid" do
+    context "Without excludeAttributes or includeAttributes parameters" do
+      before :each do
+        node = create :node
+
+        get node_path(node)
+      end
+
+      it "HTTP response code is 200 (OK)" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "Response content-type must be json" do
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it "The body should contain an array of objects" do
+        expect(JSON.parse(response.body)).to be_a(Hash)
+      end
+
+      #TODO check tests of type return
+      # it "Should have type: \"Node\" in properties" do
+      #   expect(JSON.parse(response.body)["type"]).to eq("Node")
+      # end
+    end
+
+    context "With includeAttributes parameters" do
+      before :each do
+        node = create :node
+
+        @includeAttributes = %i(
+          description
+          dataHandle
+        )
+
+        get node_path(node), {includeAttributes: @includeAttributes.join(",")}
+        @fetched_node = JSON.parse(response.body)
+      end
+
+      it "HTTP response code is 200 (OK)" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "Response content-type must be json" do
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it "The body should contain an array of objects" do
+        expect(@fetched_node).to be_a(Hash)
+      end
+
+      it "All items in response should've all the specified attributes" do
+        @includeAttributes.each do |attribute|
+          expect(@fetched_node.has_key? attribute.to_s).to eq(true)
+        end
+      end
+
+      it "All items in response shouldn't have any attribute not specified" do
+        absent = NodesHelper::required_fields - @includeAttributes
+        absent.each do |attribute|
+          expect(@fetched_node.has_key? attribute.to_s).to eq(false)
+        end
+      end
+    end
+
+    context "With excludeAttributes parameters" do
+      before :each do
+        node = create :node
+        @excludeAttributes = %i(
+          description
+          dataHandle
+        )
+
+        get node_path(node), {excludeAttributes: @excludeAttributes.join(",")}
+        @fetched_node = JSON.parse(response.body)
+      end
+
+      it "HTTP response code is 200 (OK)" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "Response content-type must be json" do
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it "The body should contain an array of objects" do
+        expect(@fetched_node).to be_a(Hash)
+      end
+
+      it "All items in response shouldn't have any of the specified attributes" do
+        @excludeAttributes.each do |attribute|
+          expect(@fetched_node.has_key? attribute.to_s).to eq(false)
+        end
+      end
+
+      it "All items in response should have all unspecified attributes" do
+        absent = NodesHelper::required_fields - @excludeAttributes
+        absent.each do |attribute|
+          expect(@fetched_node.has_key? attribute.to_s).to eq(true)
         end
       end
     end
