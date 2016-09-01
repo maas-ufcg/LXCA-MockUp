a5561ccb5d2e040f03323f5d12cbfc5d96561816
Tercio de Melo <terciodemelo@gmail.com>
2016-09-01 08:58:01 -0300

    [Nodes] Fixing routing spec

--- spec/routing/nodes_routing_spec.rb
+++ spec/routing/nodes_routing_spec.rb
@@ -7,20 +7,8 @@ RSpec.describe NodesController, type: :routing do
       expect(:get => "/nodes").to route_to("nodes#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/nodes/new").to route_to("nodes#new")
-    end
-
     it "routes to #show" do
       expect(:get => "/nodes/1").to route_to("nodes#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/nodes/1/edit").to route_to("nodes#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/nodes").to route_to("nodes#create")
     end
 
     it "routes to #update via PUT" do
@@ -30,10 +18,5 @@ RSpec.describe NodesController, type: :routing do
     it "routes to #update via PATCH" do
       expect(:patch => "/nodes/1").to route_to("nodes#update", :id => "1")
     end
-
-    it "routes to #destroy" do
-      expect(:delete => "/nodes/1").to route_to("nodes#destroy", :id => "1")
-    end
-
   end
 end
