d98564e7d579afa0a5335d54a82e6c8539e5e586
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2016-09-09 11:42:07 -0300

    starting tests

--- spec/models/cmm_spec.rb
+++ spec/models/cmm_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Cmm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validity of Cmmm Model" do
+
+
+    context "Factory :cmm" do
+      before :each do
+        @valid_cmm = build :cmm
+      end
+
+      it ":cmm factory should build a valid object" do
+        expect(@valid_cmm).to be_valid(Cmmm)
+      end
+
+       it "saving a new cmm should change the number of elements on DB" do
+         expect {@valid_cmm.save}.to change {Cmmm.count}.by 1
+       end
+
+       it "cmm._id show be equal to cmm.properties[:uuid]" do
+         expect(@valid_cmm._id).to eq(@valid_cmm.properties[:uuid])
+       end
+
+       CmmsHelper::required_fields.each do |field|
+         it "expect field #{field} can't be nil or empty string" do
+           expect(@valid_cmm.properties[field]).to_not eq(nil)
+           expect(@valid_cmm.properties[field]).to_not eq("")
+         end
+      end
+    end
+
+     CmmmsHelper::required_fields.each do |field|
+       context "Factory :no_#{field}_cmm" do
+         before :each do
+           @cmm = build :"no_#{field}_cmm"
+         end
+
+         it ":no_#{field}_cmm factory should build an invalid object" do
+           expect(@cmm).to_not be_valid(Cmmm)
+         end
+
+         it "saving cmm without #{field} should not change the number of elements in DB" do
+           expect {@cmm.save}.to change {Cmmm.count}.by 0
+         end
+       end
+     end
+  end
 end
