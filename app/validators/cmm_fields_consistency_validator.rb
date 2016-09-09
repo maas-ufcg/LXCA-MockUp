7f9b504f2960a1495b3cb5436dae86ee36361e3d
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2016-09-09 11:09:13 -0300

    finished cmm validators

--- app/validators/cmm_fields_consistency_validator.rb
+++ app/validators/cmm_fields_consistency_validator.rb
@@ -1,55 +1,13 @@
 class CmmFieldsConsistencyValidator < ActiveModel::Validator
 
-def validate(record)
-  validate_properties(record)
-  validate_id_and_uuid(record)
-  validate_fields_presence(record)
-  validate_fields_value(record)
-end
-
-private
-
-def validate_properties(record_v)
-  if record_v.nil?
-        record_v.error[:base] << "The object should not be nil"
-    elsif record_v.properties.nil?
-      record_v.error[:base] << "properties attribute should not be nil"
-    elsif not record_v.properties.is_a? Hash
-      record_v.error[:base] << "properties must be a Hash"
-    end
-end
-
-def validate_id_and_uuid(record_v)
-   if record_v.nil?
-     record_v.errors[:base] << "The UUID attribute should not be nil"
-   elsif record_v._id != record_v.properties[:uuid]
-     record_v.errors[:base] << "Object _id must be equal to properties[:_id]"
-   elsif record_v.properties[:uuid].nil? or record_v.properties[:uuid] == ""
-     record_v.errors[:base] << "The UUID atribute should neither be nil nor empty"
-     endvalidate_isAddOnCard
-   end
- end
-
- def validate_fields_presence(record_v)
-   CmmsHelper::required_fields.each do |field|
-     if not record_v.properties.has_key? field
-       record_v.errors[:base] << "missing #{field} property object"
-     elsif record_v.properties[field].nil?
-       record_v.errors[:base] << "field #{field} can't be nil"
-     elsif record_v.properties[field] == ""
-       record_v.errors[:base] << "field #{field} can't be an empty string"
+  def validate(record)
+     if record.nil?
+       record.errors[:base] << "The object cannot be nil."
+     elsif not record.properties.is_a? Hash
+       record.errors[:base] << "Properties attribute must be a Hash (actual: #{record.properties.class})"
+     elsif record._id != record.properties[:uuid]
+       record.errors[:base] << "Object _id must be equal to properties[:uuid]
+       (actual _id: #{record._id} and uuid: #{record.properties[:uuid]})"
      end
    end
  end
-
-
- def validate_fields_value(record_v)
-     values_per_field = CmmsHelper::possible_values_per_field
-     values_per_field.keys.take(10).each do |field|
-       if not values_per_field[field].include? record_v.properties[field]
-         record_v.errors[:base] << "field #{field} can't have value #{record_v.properties[field]})"
-       end
-     end
-   end
-
-end
---
+++ app/validators/cmm_properties_validator.rb
@@ -0,0 +1,83 @@
+class CmmPropertiesValidator < ActiveModel::Validator
+
+  def validate(record)
+    if record.nil?
+      record.errors[:base] << "Record cannot be nil"
+    end
+
+    @record = record
+    @valid_values = CmmsHelper::possible_values_per_field
+
+    validate_type(:accessState, String)
+    validate_values(:accessState, @valid_values[:accessState])
+
+    validate_type(:backedBy, String)
+    validate_values(:backedBy, @valid_values[:backedBy])
+
+    validate_type(:cmmDisplayName, String)
+
+    validate_type(:cmmHealthState, String)
+    validate_values(:cmmHealthState, @valid_values[:cmmHealthState])
+
+    validate_type(:dataHandle, Fixnum)
+    validate_type(:description, String)
+    validate_type(:dnsHostName, Array)
+    validate_type(:domainName, String)
+
+    validate_type(:errorFields, Array)
+
+    validate_type(:excludedHealthState, String)
+    validate_values(:excludedHealthState, @valid_values[:excludedHealthState])
+
+    validate_type(:firmware, Array)
+    validate_type(:FRU, String)
+    validate_type(:fruSerialNumber, String)
+    validate_type(:hostConfig, Array)
+
+    validate_type(:hostname, String)
+    validate_type(:ipInterfaces, Array)
+
+    validate_type(:ipv4Addresses, Array)
+    validate_type(:ipv6Addresses, Array)
+    validate_type(:leds, Array)
+    validate_type(:macAddresses, Array)
+    validate_type(:machineType, String)
+    validate_type(:mgmtProcIPaddress, String)
+    validate_type(:model, String)
+
+    validate_type(:name, String)
+    validate_values(:name, @valid_values[:name])
+
+    validate_type(:overallHealthState, String)
+    validate_type(:parent, Hash)
+    validate_type(:partNumber, String)
+    validate_type(:powerAllocation, Hash)
+    validate_type(:productID, String)
+
+    validate_type(:role, Array)
+    validate_values(:role, @valid_values[:role])
+
+    validate_type(:serialNumber, String)
+    validate_type(:slots, Integer)
+    validate_type(:type, String)
+    validate_type(:userDescription, String)
+    validate_type(:uri, String)
+    validate_type(:uuid, String)
+
+  end
+
+  private
+
+  def validate_type(attribute_key, type)
+    if not @record.properties[attribute_key].is_a? type
+      @record.errors[:base] << "#{attribute_key} attribute should be a #{type} (actual:#{object.class})"
+    end
+  end
+
+  def validate_values(attribute_key, possible_values)
+    if not possible_values.include? @record.properties[attribute_key]
+      @record.errors[:base] << "#{attribute_key} attribute is not valid"
+    end
+  end
+
+end
