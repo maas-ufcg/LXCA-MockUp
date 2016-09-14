87a8788f191339a01620c6be0bffef2b4da60e9e
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2016-09-13 21:38:17 -0300

    Adding nodes tests :)

--- spec/models/node/node_nil_fields_spec.rb
+++ spec/models/node/node_nil_fields_spec.rb
@@ -1,23 +1,23 @@
-# require 'rails_helper'
-#
-# RSpec.describe Node, type: :model do
-#   describe "Database Data Validation: " do
-#
-#     NodesHelper::required_fields.each do |field|
-#       context "Factory :nil_#{field}_node" do
-#         before :each do
-#           @node = build :"nil_#{field}_node"
-#         end
-#
-#         it ":nil_#{field}_node factory should not build a valid object" do
-#           expect(@node).to_not be_valid(Node)
-#         end
-#
-#         it "saving fan without #{field} should not change the number of elements in DB" do
-#           expect {@node.save}.to change {Node.count}.by 0
-#         end
-#
-#       end
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Node, type: :model do
+  describe "Database Data Validation: " do
+
+    NodesHelper::required_fields.each do |field|
+      context "Factory :nil_#{field}_node" do
+        before :each do
+          @node = build :"nil_#{field}_node"
+        end
+
+        it ":nil_#{field}_node factory should not build a valid object" do
+          expect(@node).to_not be_valid(Node)
+        end
+
+        it "saving fan without #{field} should not change the number of elements in DB" do
+          expect {@node.save}.to change {Node.count}.by 0
+        end
+
+      end
+    end
+  end
+end
--- spec/models/node/node_required_fields_spec.rb
+++ spec/models/node/node_required_fields_spec.rb
@@ -1,18 +1,18 @@
-# require 'rails_helper'
-#
-# RSpec.describe Node, type: :model do
-#   describe "Database Data Validation: " do
-#     context "Properties fields existence" do
-#       before :each do
-#         @valid_node = build :node
-#       end
-#
-#       NodesHelper::required_fields.each do |field|
-#         it "expect field #{field} neither can't be nil nor empty string" do
-#           expect(@valid_node.properties[field]).to_not eq(nil)
-#           expect(@valid_node.properties[field]).to_not eq("")
-#         end
-#       end
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Node, type: :model do
+  describe "Database Data Validation: " do
+    context "Properties fields existence" do
+      before :each do
+        @valid_node = build :node
+      end
+
+      NodesHelper::required_fields.each do |field|
+        it "expect field #{field} neither can't be nil nor empty string" do
+          expect(@valid_node.properties[field]).to_not eq(nil)
+          expect(@valid_node.properties[field]).to_not eq("")
+        end
+      end
+    end
+  end
+end
