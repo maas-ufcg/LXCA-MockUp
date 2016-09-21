940d3305356090302473137bba6b80a68c48d76a
maysasouza <maysa.souza@ccc.ufcg.edu.br>
2016-09-21 11:35:01 -0300

    [/ScalableComplex] fixing tests

--- app/controllers/scalable_complexes_controller.rb
+++ app/controllers/scalable_complexes_controller.rb
@@ -5,15 +5,7 @@ class ScalableComplexesController < ApplicationController
   #GET /scalable_complex
   #GET /scalable_complex.json
   def index
-    param = params[:status]
-    formatType = params[:formatType]
-    if param.nil?
-        @scalableComplexes = ScalableComplex.all.to_a
-    else
-        @scalableComplexes = Switch.all.to_a.select do |scalableComplexes|
-      scalableComplexes.properties.to_hash.deep_symbolize_keys[:status][:message] == param
-    end
-  end
+    @scalableComplexes = ScalableComplex.all.to_a
     render(json: @scalableComplexes.map do |scalableComplex|
       setup_scalableComplex_properties scalableComplex
       scalableComplex.properties
@@ -26,9 +18,10 @@ class ScalableComplexesController < ApplicationController
     if @scalableComplex.nil?
       head :not_found
     else
-      render(json: @scalableComplex.map {|n|
-        setup_scalableComplex_properties n
-        n.properties })
+      render json: @scalableComplex.properties
+      # render(json: @scalableComplex.map {|n|
+      #   setup_scalableComplex_properties n
+      #   n.properties })
     end
   end
 
@@ -36,21 +29,18 @@ class ScalableComplexesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_scalableComplex
+
     @scalableComplex = begin
-              params[:id].split(',').map do |id|
-                    ScalableComplex.find(id)
-                end
-                rescue Mongoid::Errors::DocumentNotFound => ex
-              end
-    setup_scalableComplex_properties @scalableComplex
+        ScalableComplex.find(params[:id])
+        rescue Mongoid::Errors::DocumentNotFound => ex
+        end
+        setup_scalableComplex_properties @scalableComplex
   end
 
   def setup_scalableComplex_properties(scalableComplex)
     return if scalableComplex.nil?
     excludeAttributes = split_to_sym params[:excludeAttributes]
     includeAttributes = split_to_sym params[:includeAttributes]
-
-
 
     unless includeAttributes.nil?
       excludeAttributes = ScalableComplexHelper::required_fields-includeAttributes
