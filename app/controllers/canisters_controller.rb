723709dfc29ab3a09ee2e7bdecfa0c1edacadb24
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2016-09-27 13:28:43 -0300

    fixing controller

--- app/controllers/canisters_controller.rb
+++ app/controllers/canisters_controller.rb
@@ -4,7 +4,15 @@ class CanistersController < ApplicationController
   # GET /canisters
   # GET /canisters.json
   def index
-    @canisters = Canister.all.to_a
+    param = params[:status]
+    formatType = params[:formatType]
+    if param.nil?
+      @canisters = Canister.all.to_a
+    else
+      @canisters = Canister.all.to_a.select do |canister|
+        canister.properties.to_hash.deep_symbolize_keys[:status][:message] == param
+      end
+    end
     render(json: @canisters.map do |canister|
       setup_canister_properties canister
       canister.properties
@@ -14,27 +22,33 @@ class CanistersController < ApplicationController
   # GET /canisters/1
   # GET /canisters/1.json
   def show
-    if @canister.nil?
+    if @canisters.nil?
       head :not_found
     else
-      render json: @canister.properties
+      render(json: @canisters.map {|n|
+        setup_canister_properties n
+        n.properties })
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_canister
-      @canister = begin
-               Canister.find(params[:id])
-             rescue Mongoid::Errors::DocumentNotFound => ex
-             end
-      setup_canister_properties @canister
+      @canisters = begin
+                 params[:id].split(',').map do |id|
+                   Canister.find(id)
+                 end
+               rescue Mongoid::Errors::DocumentNotFound => ex
+               end
     end
 
     def setup_canister_properties(canister)
       return if canister.nil?
       excludeAttributes = split_to_sym params[:excludeAttributes]
       includeAttributes = split_to_sym params[:includeAttributes]
+
+
+
       unless includeAttributes.nil?
         excludeAttributes = CanistersHelper::required_fields-includeAttributes
       end
@@ -49,9 +63,25 @@ class CanistersController < ApplicationController
       string.split(",").map(&:to_sym) if string.is_a?(String)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def canister_params
-      params.require(:canister).permit(:_id, :properties)
+    def setup_canister_properties(canister)
+      return if canister.nil?
+      excludeAttributes = split_to_sym params[:excludeAttributes]
+      includeAttributes = split_to_sym params[:includeAttributes]
+
+
+
+      unless includeAttributes.nil?
+        excludeAttributes = CanistersHelper::required_fields-includeAttributes
+      end
+      unless excludeAttributes.nil?
+        excludeAttributes.each do |attribute|
+          canister.properties.delete(attribute)
+        end
+      end
+    end
+
+    def split_to_sym(string)
+      string.split(",").map(&:to_sym) if string.is_a?(String)
     end
 
 end
