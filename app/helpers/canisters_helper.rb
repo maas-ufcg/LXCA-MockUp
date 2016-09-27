5282650a0d2c9f5b04e02ff0c8464918de6c91d7
Lucas Silva <lucas.silva@ccc.ufcg.edu.br>
2016-09-27 13:54:55 -0300

    [/canisters] fixing include attributes request

--- app/helpers/canisters_helper.rb
+++ app/helpers/canisters_helper.rb
@@ -1,11 +1,12 @@
 module CanistersHelper
   def self.required_fields
     %i(
-      cmmDisplayName backedBy contact dataHandle  description
-      firmware FRU domainName driveBays drives errorFields ipInterfaces powerStatus
-      fruSerialNumber manufacturer manufacturerID processorSlots
-      processors name parent posID productId productName
-      serviceHostName userDescription uri vnicMode uuid vpdID
+    activationKeys bladeState cmmDisplayName cmmHealthState backedBy contact dataHandle  description
+   firmware FRU domainName driveBays drives errorFields ipInterfaces powerStatus ipv6Addresses
+   fruSerialNumber manufacturer manufacturerID processorSlots leds ipv4Addresses hostname
+   processors name parent posID productId productName model location lanOverUsb ipv4ServiceAddress
+   serviceHostName userDescription uri vnicMode uuid vpdID macAddress ipv6ServiceAddress
+   subSlots subType slots serialNumber posId partNumber memorySlots memoryModules machineType
     )
   end
 end
