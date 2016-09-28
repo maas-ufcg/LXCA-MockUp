bd4e25878f6599b75b1ae3e6b5cdc0055440e6d4
Gustavo Diniz <gustavo.monteiro@ccc.ufcg.edu.br>
2016-09-28 14:34:57 -0300

    finalized

--- app/helpers/canisters_helper.rb
+++ app/helpers/canisters_helper.rb
@@ -1,12 +1,11 @@
 module CanistersHelper
   def self.required_fields
     %i(
-    activationKeys bladeState cmmDisplayName cmmHealthState backedBy contact dataHandle  description
-   firmware FRU domainName driveBays drives errorFields ipInterfaces powerStatus ipv6Addresses
-   fruSerialNumber manufacturer manufacturerID processorSlots leds ipv4Addresses hostname
-   processors name parent posID productId productName model location lanOverUsb ipv4ServiceAddress
-   serviceHostName userDescription uri vnicMode uuid vpdID macAddress ipv6ServiceAddress
-   subSlots subType slots serialNumber posId partNumber memorySlots memoryModules machineType
+    cmmDisplayName backedBy contact dataHandle  description
+      firmware FRU domainName driveBays drives errorFields ipInterfaces powerStatus
+      fruSerialNumber manufacturer manufacturerID processorSlots
+      processors name parent posID productId productName
+      serviceHostName userDescription uri vnicMode uuid vpdID
     )
   end
 end
